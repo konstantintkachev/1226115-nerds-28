2d5bef3cc6d7c4a53a5289df79bbc28d686c291f
Konstantin <konstantintkachev92@gmail.com>
2020-07-06 23:58:58 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,16 +1,3 @@
-var form = document.querySelector(".a-popup__form");
-var popup = document.querySelector(".a-popup");
-var close = document.querySelector(".a-popup__close-cross");
-var login = document.querySelector("a-popup__login");
 
-form.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  popup.classList.add("a-popup__show");
-  login.focus();
-});
-
-close.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  popup.classList.remove("a-popup__show");
-});
+var link = document.querySelector(".popup__btn");
 
